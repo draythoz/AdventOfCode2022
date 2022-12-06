3d3473bbae9a69d565b2c49da608ad7b5abcf0df
Eric Ott <draythoz@gmail.com>
2022-12-06 17:41:09 -0500

    Day 6 Part 1

---
+++ Day6Part1/Program.cs
@@ -0,0 +1,35 @@
+﻿char ch;
+var startWindow = new char[4] {default(char),default(char),default(char),default(char)};
+StreamReader reader;
+reader = new StreamReader(@"input.txt");
+var characterIndex = 0;
+do
+{
+    ch = (char)reader.Read();
+    Array.Copy(startWindow, 1, startWindow, 0, startWindow.Length - 1);
+    startWindow[3] = ch;
+    characterIndex++;
+} while (!reader.EndOfStream && !AllUniqueChars(startWindow));
+
+reader.Dispose();
+Console.WriteLine(startWindow);
+Console.WriteLine(characterIndex);
+
+bool AllUniqueChars(char[] stringToTest)
+{
+    if (stringToTest[0] == default(char)) return false;
+    
+    for (int i = 0; i < stringToTest.Length; i++)
+    {
+        var currentChar = stringToTest[i];
+        for (int j = 0; j < stringToTest.Length; j++)
+        {
+            if (i == j)
+                continue;
+
+            if (currentChar == stringToTest[j]) return false;
+        }
+    }
+
+    return true;
+}

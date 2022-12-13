bc69bfad10f69a58983058fe6485518229f266e0
Eric Ott <draythoz@gmail.com>
2022-12-12 23:09:16 -0500

    Day9Part2 complete

--- Day9Part2/Program.cs
+++ Day9Part2/Program.cs
@@ -1,7 +1,10 @@
-﻿var lines = File.ReadLines("testinput.txt");
-
+﻿var lines = File.ReadLines("input.txt");
+const int segments = 10;
+const int finalTailIndex = segments - 2;
 var head = new Position(0, 0);
-var tail = new Position(0, 0);
+var tails = new Position[segments - 1];
+for (var i = 0; i <= finalTailIndex; i++) tails[i] = new Position(0, 0);
+
 var visitedPositions = new HashSet<string> { "0:0" };
 
 foreach (var line in lines)
@@ -13,8 +16,11 @@ foreach (var line in lines)
     for (var i = 0; i < numberOfSpaces; i++)
     {
         MoveHead(ref head, direction);
-        CatchUpTail(ref head, ref tail);
-        visitedPositions.Add(tail.ToString());
+        CatchUpTail(ref head, ref tails[0]);
+        for (var t = 1; t <= finalTailIndex; t++) CatchUpTail(ref tails[t - 1], ref tails[t]);
+
+        var finalTailPosition = tails[finalTailIndex].ToString();
+        visitedPositions.Add(finalTailPosition);
     }
 }
 
@@ -24,6 +30,10 @@ foreach (var line in lines)
 // }
 
 Console.WriteLine($"Number of visited positions : {visitedPositions.Count}");
+// foreach (var position in visitedPositions)
+// {
+//     Console.WriteLine(position);
+// }
 
 void MoveHead(ref Position head, string direction)
 {
@@ -49,31 +59,46 @@ void CatchUpTail(ref Position head, ref Position tail)
     var xDist = head.X - tail.X; //4,1
     var yDist = head.Y - tail.Y; //2,0
 
-    var moveDiagonally = Math.Abs(xDist) + Math.Abs(yDist) >= 3;
+    var absoluteDistance = Math.Abs(xDist) + Math.Abs(yDist);
+    var moveDiagonally = absoluteDistance >= 3;
+
+    if (moveDiagonally)
+    {
+        MoveTailDiagonally(xDist, yDist, ref tail);
+        return;
+    }
 
     if (xDist > 1)
     {
         tail.MoveRight();
-        if (moveDiagonally) tail.Y = head.Y;
+        return;
     }
 
     if (xDist < -1)
     {
         tail.MoveLeft();
-        if (moveDiagonally) tail.Y = head.Y;
+        return;
     }
 
     if (yDist > 1)
     {
         tail.MoveUp();
-        if (moveDiagonally) tail.X = head.X;
+        return;
     }
 
     if (yDist < -1)
     {
         tail.MoveDown();
-        if (moveDiagonally) tail.X = head.X;
     }
+}
+
+void MoveTailDiagonally(int xDist, int yDist, ref Position tail)
+{
+    if (xDist > 0) tail.MoveRight();
+    else tail.MoveLeft();
+
+    if (yDist > 0) tail.MoveUp();
+    else tail.MoveDown();
 }
 
 internal class Position
